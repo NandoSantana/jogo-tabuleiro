3ffe18e832576611ccecf144d048886b237351a8
Fernando Santana <bh.carvalho@gmail.com>
2023-09-17 03:19:59 -0300

    capture

--- src/components/BoardDama.vue
+++ src/components/BoardDama.vue
@@ -2,13 +2,22 @@
   <div class="board">
     <table>
       <tbody>
+      Vez de {{currentPlayer}} <br/>-  Jogadas X = {{playerXScore}} <br/>- Jogadas O = {{playerOScore}}
       <tr v-for="(row, rowIndex) in board" :key="rowIndex">
-
-          <td v-for="(piece, colIndex) in row" class="cell" :key="colIndex">
+          <td v-for="(piece, colIndex) in row"
+              class="cell"
+              :key="colIndex"
+              @click="handleCellClick(rowIndex, colIndex, piece)"
+              :class="{
+                selectable: isSelectedPiece(rowIndex, colIndex),
+                 piece: true, // Classe básica para todas as células
+                // selectable: isSelectable(rowIndex, colIndex), // Para células selecionáveis
+                empty: piece === null, // Para células vazias
+                player1: piece === 'X', // Para peças do jogador 1
+                player2: piece === 'O' // Para peças do jogador 2
+              }">
             <!-- evento drag n drop-->
             <div
-                @click="handleCellClick(rowIndex, colIndex, piece)"
-                :class="{ selectable: isSelectable(rowIndex, colIndex) }"
             >
               {{ piece }}
             </div>
@@ -24,55 +33,30 @@
 <script>
 export default {
   name: "Board-dama",
-  // props: ['board'],
   data() {
-    // const board = [
-    //   [null, { piece: 'B', player: 'Player 1' }, null, { piece: 'B', player: 'Player 1' }, null, { piece: 'B', player: 'Player 1' }, null, { piece: 'B', player: 'Player 1' }],
-    //   [{ piece: 'B', player: 'Player 1' }, null, { piece: 'B', player: 'Player 1' }, null, { piece: 'B', player: 'Player 1' }, null, { piece: 'B', player: 'Player 1' }, null],
-    //   [null, { piece: 'B', player: 'Player 1' }, null, { piece: 'B', player: 'Player 1' }, null, { piece: 'B', player: 'Player 1' }, null, { piece: 'B', player: 'Player 1' }],
-    //   [null, null, null, null, null, null, null, null],
-    //   [null, null, null, null, null, null, null, null],
-    //   [{ piece: 'W', player: 'Player 2' }, null, { piece: 'W', player: 'Player 2' }, null, { piece: 'W', player: 'Player 2' }, null, { piece: 'W', player: 'Player 2' }, null],
-    //   [null, { piece: 'W', player: 'Player 2' }, null, { piece: 'W', player: 'Player 2' }, null, { piece: 'W', player: 'Player 2' }, null, { piece: 'W', player: 'Player 2' }],
-    //   [{ piece: 'W', player: 'Player 2' }, null, { piece: 'W', player: 'Player 2' }, null, { piece: 'W', player: 'Player 2' }, null, { piece: 'W', player: 'Player 2' }, null],
-    // ];
-
-    // const board = [
-    //   [null, 'B', null, 'B', null, 'B', null, 'B'],
-    //   ['B', null, 'B', null, 'B', null, 'B', null],
-    //   [null, 'B', null, 'B', null, 'B', null, 'B'],
-    //   ['', null, '', null, '', null, '', null],
-    //   [null, '', null, '', null, '', null, ''],
-    //   ['W', null, 'W', null, 'W', null, 'W', null],
-    //   [null, 'W', null, 'W', null, 'W', null, 'W'],
-    //   ['W', null, 'W', null, 'W', null, 'W', null],
-    // ];
-
     const board = [
       ['O', null, 'O', null, 'O', null, 'O', null],
       [null, 'O', null, 'O', null, 'O', null, 'O'],
       ['O', null, 'O', null, 'O', null, 'O', null],
+
       [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+
       [null, 'X', null, 'X', null, 'X', null, 'X'],
       ['X', null, 'X', null, 'X', null, 'X', null],
       [null, 'X', null, 'X', null, 'X', null, 'X'],
     ];
-    // Exemplo de como validar se uma peça pertence ao "Player 2" em uma posição específica (row, col)
-    // const row = 6; // Exemplo: verificar a peça na linha 6
-    // const col = 1; // Exemplo: verificar a peça na coluna 1
-
-    // if (board[row][col] && board[row][col].player === 'Player 2') {
-    //   // A peça na posição (row, col) pertence ao "Player 2"
-    //   console.log('Esta peça pertence ao Player 2.');
-    // } else {
-    //   // A peça não pertence ao "Player 2" ou a posição está vazia
-    //   console.log('Esta peça não pertence ao Player 2 ou a posição está vazia.');
-    // }
 
     return {
       board: board, // Matriz que representa o tabuleiro
       selectedPiece: null, // Armazena a peça selecionada
-      currentPlayer: 'player1', // Define o jogador atual
+      currentPlayer: 'X', // Define o jogador atual
+      lastOpponent: 'O',
+      validMoves: [],
+      selectedCell: null,
+      playerXScore: 0,
+      playerOScore: 0,
+      capturado: null,
       // Outras informações do jogo aqui
     };
 
@@ -80,29 +64,155 @@ export default {
   methods: {
     handleCellClick(row, col) {
       const piece = this.board[row][col];
-    console.log('peça',piece);
-      // Verificar se a célula clicada contém uma peça do jogador atual
-      if (piece === 'X' || piece === 'O') {
-        // if (!this.selectedPiece) {
-          // Se nenhuma peça estiver selecionada, selecione a peça clicada
-          this.selectedPiece = { row, col };
-          console.log(this.selectedPiece);
 
-          // Caso contrário, verifique se o movimento é válido e mova a peça
-          console.log('valid?',this.isValidMove(this.selectedPiece, row, col));
-          if (this.isValidMove(this.selectedPiece, row, col)) {
-            this.movePiece(this.selectedPiece, row, col);
-            this.selectedPiece = null; // Desselecione a peça
+      if (piece === this.currentPlayer) {
+
+        // Se o jogador clicou em sua própria peça, selecione-a
+        this.selectedCell = { row, col, piece };
+        this.selectedPiece = { row, col, piece };
+        console.log('Selected', this.selectedPiece)
+        // Continue com o movimento da peça como anteriormente
+        // this.movePiece(this.selectedCell, row, col);
+        // this.selectedCell = null;
+      } else if ( this.selectedPiece ) {
+        // Verifique se o movimento é válido e realize a captura de peças
+          this.movePiece(this.selectedPiece, row, col);
+          this.capturePiece(row, col);
+
+          // Continue com o movimento da peça como anteriormente
+          this.selectedPiece = null;
+          // console.log('capturado', this.capturado)
+          // Alterne entre os jogadores
+          this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+      }
+    },
+    capture(fromRow, fromCol, toRow, toCol) {
+      const opponent = this.currentPlayer === 'X' ? 'O' : 'X';
+
+      // Verifique se a célula de destino está vazia
+      if (this.board[toRow][toCol] === null) {
+        // Verifique se há uma peça do oponente na célula intermediária
+        const middleRow = (fromRow + toRow) / 2;
+        const middleCol = (fromCol + toCol) / 2;
+
+        if (this.board[middleRow][middleCol] === opponent) {
+          // Realize a captura de peça:
+          // 1. Limpe a célula de origem
+          this.board[fromRow][fromCol] = null;
+
+          // 2. Limpe a célula do oponente
+          this.board[middleRow][middleCol] = null;
+
+          // 3. Mova a peça para a célula de destino
+          this.board[toRow][toCol] = this.currentPlayer;
+
+          // Atualize a pontuação do jogador
+          if (this.currentPlayer === 'X') {
+            this.playerXScore++;
           } else {
-            // Se o movimento não for válido, mantenha a peça selecionada
-            this.selectedPiece = { row, col };
+            this.playerOScore++;
           }
 
-      } else if (this.selectedPiece) {
-        // Se uma peça já estiver selecionada e o jogador clicar em uma célula vazia,
-        // desselecione a peça
-        this.selectedPiece = null;
+          // Alterne entre os jogadores
+          this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+        }
       }
+    },
+    // capturePiece(selectedPiece, targetRow, targetCol) {
+    //   // Realize a captura de peças (remova a peça do adversário)
+    //   // console.log(this.board[selectedPiece.row][selectedPiece.col].piece);
+    //   // const piece = this.board[selectedCell.row][selectedCell.col].piece;
+    //   const opponent = this.currentPlayer === 'X' ? 'O' : 'X';
+    //   console.log('choose', {targetRow, targetCol});
+    //   console.log('board', this.board);
+    //
+    //   // console.log('opponent',opponent)
+    //   if (selectedPiece.piece !== opponent) {
+    //     console.log('engoliu linha',  this.board[targetRow]);
+    //     console.log('engoliu coluna',  targetCol);
+    //
+    //     this.board[targetRow].forEach( el => {
+    //       console.log(el);
+    //       this.board[targetRow][targetCol] = selectedPiece.piece;
+    //     });
+    //     console.log('engoliu',  this.board[targetRow[targetCol]]);
+    //     // this.board[targetRow][targetCol] = selectedPiece.piece;
+    //     this.capturado = {targetRow, targetCol};
+    //
+    //     if (this.currentPlayer === 'O') {
+    //       this.playerXScore++;
+    //     } else {
+    //       this.playerOScore++;
+    //     }
+    //
+    //     // return this.board[targetRow][targetCol];
+    //   }
+    // },
+
+    capturePiece(row, col) {
+      // Verifique se o jogador atual é 'X' ou 'O' e identifique o oponente
+      const currentPlayer = this.currentPlayer;
+      console.log('currentPlayer', currentPlayer)
+      const opponent = currentPlayer === 'X' ? 'O' : 'X';
+
+      // Calcule a posição da célula intermediária
+      const middleRow = Math.round((this.selectedPiece.row + row) / 2);
+      const middleCol = Math.round((this.selectedPiece.col + col) / 2);
+
+      // Verifique se a célula intermediária contém uma peça do oponente
+
+      if ( this.board[this.selectedPiece.row][this.selectedPiece.col] !== opponent ) {
+
+        // Limpe a célula intermediária (peça do oponente capturada)
+        this.board[this.selectedPiece.row][this.selectedPiece.col] = null;
+        this.board[middleRow][middleCol] = null;
+
+        // Mova a peça para a célula de destino
+        this.board[row][col] = currentPlayer;
+
+        // Atualize a pontuação do jogador atual
+        if (currentPlayer === 'X') {
+          this.playerXScore++;
+        } else {
+          this.playerOScore++;
+        }
+
+
+      }
+    },
+
+    calculateValidMoves(selectedPiece) {
+      this.validMoves.pop();
+      const validMoves = [];
+      // const currentPlayer = selectedPiece.player;
+      const currentRow = selectedPiece.row;
+      const currentCol = selectedPiece.col;
+
+      // Verifique as células diagonais adjacentes
+      // Adicione-as à lista de movimentos válidos se forem válidas
+      const directions = [
+        { row: -1, col: -1 },
+        { row: -1, col: 1 },
+        // Adicione mais direções de acordo com as regras do seu jogo
+      ];
+
+      for (const dir of directions) {
+        const newRow = currentRow + dir.row;
+        const newCol = currentCol + dir.col;
+
+        // Verifique se a nova posição está dentro do tabuleiro
+        if (newRow >= 0 && newRow < this.board.length && newCol >= 0 && newCol < this.board[0].length) {
+          // const cell = this.board[newRow][newCol];
+
+          // Verifique se a célula está vazia e se o movimento é válido
+          // if (!cell && this.isValidMove(currentPlayer, currentRow, currentCol, newRow, newCol)) {
+            validMoves.push({ row: newRow, col: newCol });
+          // }
+        }
+      }
+
+      this.validMoves = validMoves;
+
     },
     isSelectable(row, col) {
       // Verificar se a célula é selecionável (ou seja, se contém uma peça do jogador atual)
@@ -114,44 +224,60 @@ export default {
     },
     isSelectedPiece(row, col) {
       // Verifique se a posição atual é a peça selecionada
+
+      // this.calculateValidMoves(this.selectedPiece)
       return (
           this.selectedPiece &&
           this.selectedPiece.row === row &&
           this.selectedPiece.col === col
       );
     },
+    isSelectoGo() {
+      // Verifique se a posição atual é a peça selecionada
+
+      this.calculateValidMoves(this.selectedPiece)
+      return true;
+    },
     isValidMove(selectedPiece, targetRow, targetCol) {
-      // Implemente a lógica para verificar se o movimento é válido
-      // Isso depende das regras do jogo de damas
-      // Exemplo simples: permitir movimento diagonal para frente
-      // para uma posição vazia
-      console.log('is valid to move?', {targetRow, targetCol },selectedPiece.row, selectedPiece.col)
-      // const piece = selectedPiece.piece;
       const currentRow = selectedPiece.row;
       const currentCol = selectedPiece.col;
+      const piece = this.board[currentRow][currentCol];
+      // Verifique se o movimento é diagonal para frente (depende do jogador atual)
+      const isForwardMove = piece === 'X'
+          ? targetRow > currentRow // Para o jogador 'X', movimento para linhas maiores
+          : targetRow < currentRow; // Para o jogador 'O', movimento para linhas menores
 
-      // Lógica simples para um movimento válido
-      return (
-          Math.abs(targetRow - currentRow) === 1 &&
-          Math.abs(targetCol - currentCol) === 1 &&
-          this.board[targetRow][targetCol] === null
-      );
+      // Verifique se o movimento é diagonal (diferença nas linhas e colunas é 1)
+      const isDiagonalMove = Math.abs(targetRow - currentRow) === 1 &&
+          Math.abs(targetCol - currentCol) === 1;
+
+      // Verifique se a célula de destino está vazia
+      const isTargetEmpty = this.board[targetRow][targetCol] === null;
+
+      // Verifique se todas as condições são atendidas para um movimento válido
+      return isForwardMove && isDiagonalMove && isTargetEmpty;
     },
     movePiece(selectedPiece, targetRow, targetCol) {
-      console.log('tentando mover');
       // Implemente a lógica para mover a peça no tabuleiro
       // Atualize a matriz board com o novo estado do jogo
       // Isso também depende das regras do jogo de damas
       // Exemplo simples: apenas atualize a matriz com a nova posição
-      const piece = selectedPiece.piece;
-      const currentRow = selectedPiece.row;
-      const currentCol = selectedPiece.col;
+      this.capturado = {targetRow, targetCol};
 
-      // Remova a peça da posição atual
-      this.board[currentRow][currentCol] = null;
+      if(this.isValidMove(selectedPiece, targetRow, targetCol)){
+        const piece = selectedPiece.piece;
+        const currentRow = selectedPiece.row;
+        const currentCol = selectedPiece.col;
 
-      // Coloque a peça na nova posição
-      this.board[targetRow][targetCol] = piece;
+        // Remova a peça da posição atual
+        this.board[currentRow][currentCol] = null;
+
+        // Coloque a peça na nova posição
+        this.board[targetRow][targetCol] = piece;
+        // return true
+      }
+      // return false;
+
     },
 
   },
@@ -164,14 +290,40 @@ export default {
 .board {
   /* Estilize o tabuleiro de acordo com suas preferências */
 }
-.selectable {
-  background-color: yellow;
+.toGo{
+  background-color: #42b983;
 }
+
+/* Estilos básicos para todas as células */
 .cell {
   width: 50px;
   height: 50px;
-  background-color: white;
+  text-align: center;
+  vertical-align: middle;
+  font-size: 18px;
   border: 1px solid #000;
-  /* Estilize as casas do tabuleiro de acordo com suas preferências */
 }
+
+/* Estilo para células selecionáveis (por exemplo, movimentos válidos) */
+.selectable {
+  background-color: yellow;
+}
+
+/* Estilo para células vazias */
+.empty {
+  background-color: #fff;
+}
+
+/* Estilo para peças do jogador 1 (por exemplo, peças pretas) */
+.player1 {
+  background-color: #000;
+  color: #fff;
+}
+
+/* Estilo para peças do jogador 2 (por exemplo, peças brancas) */
+.player2 {
+  background-color: yellowgreen;
+  color: #000;
+}
+
 </style>
